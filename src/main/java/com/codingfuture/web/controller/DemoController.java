1275a6a4614746c6c4c0411944303b78265b1520
wangchen <2805268151@qq.com>
2021-09-12 23:49:06 +0800

    second commit

--- src/main/java/com/codingfuture/web/controller/DemoController.java
+++ src/main/java/com/codingfuture/web/controller/DemoController.java
@@ -1,18 +1,31 @@
 package com.codingfuture.web.controller;
 
 import com.codingfuture.entity.Person;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
+import java.util.Date;
+
 @Controller
 public class DemoController {
     @ResponseBody
+    //请求路径
     @RequestMapping("/test")
-    public Person test(){
+    //@DateTimeFormat 时间字符串格式注解
+    public Person test(String name, @DateTimeFormat(pattern = "yyyy-MM-dd") Date date, int[] ids) {
         Person person = new Person();
-        person.setName("wang");
+        person.setName(name);
         person.setSex("nan");
+        System.out.println(date);
+        System.out.println(Arrays.toString(ids));
         return person;
+    }
+
+    @RequestMapping("/test2")
+    public String test2() {
+        return "";
     }
 }
