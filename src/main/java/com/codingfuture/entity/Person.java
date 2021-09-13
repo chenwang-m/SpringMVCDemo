100dd0bd5e19442fc112445ed47426bd07fcc06d
wangchen <2805268151@qq.com>
2021-09-13 18:13:30 +0800

    three commit

--- src/main/java/com/codingfuture/entity/Person.java
+++ src/main/java/com/codingfuture/entity/Person.java
@@ -1,8 +1,18 @@
 package com.codingfuture.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
 public class Person {
+    private Integer id;
     private String name;
     private String sex;
+    //时间格式转换
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
 
     public Person() {
     }
@@ -26,5 +36,30 @@ public class Person {
 
     public void setSex(String sex) {
         this.sex = sex;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", sex='" + sex + '\'' +
+                '}';
     }
 }
--- src/main/java/com/codingfuture/web/controller/DemoController.java
+++ src/main/java/com/codingfuture/web/controller/DemoController.java
@@ -3,13 +3,14 @@ package com.codingfuture.web.controller;
 import com.codingfuture.entity.Person;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.util.Arrays;
 import java.util.Date;
 
 @Controller
+@RequestMapping("/person")
 public class DemoController {
     @ResponseBody
     //请求路径
@@ -24,8 +25,48 @@ public class DemoController {
         return person;
     }
 
-    @RequestMapping("/test2")
-    public String test2() {
-        return "";
+    //通过session对象得到session中存放的对象
+    @ResponseBody
+    //@RequestMapping(value = "/test2",method = RequestMethod.GET)
+    @GetMapping("/test2")
+    public String test2(HttpSession httpSession) {
+        httpSession.setAttribute("count", 1);
+        int count = (int) httpSession.getAttribute("count");
+        System.out.println(count);
+        return "ok";
+    }
+
+    @ResponseBody
+    @GetMapping("/test3")
+    //@RequestParam 该注解用来标注一个请求参数
+    //defaultValue = "no name"
+    public String test3(@RequestParam(value = "user_name", required = false, defaultValue = "no_name") String name) {
+        return name;
+    }
+
+    //RESTful 请求方式
+    //@PathVariable 将路径的一部分作为请求参数
+    @ResponseBody
+    @RequestMapping(value = {"/test4/{id}", "/test4-1/{id}"})
+    public Person test4(@PathVariable Integer id) {
+        Person person = new Person();
+        person.setId(id);
+        person.setName("666");
+        person.setSex("nan");
+        return person;
+    }
+
+    @ResponseBody
+    @RequestMapping("/test5")
+    public String test5(@RequestBody Person person) {
+        System.out.println(person);
+        return "OK";
+    }
+
+    @ResponseBody
+    @RequestMapping("/test6")
+    public Person test6(@RequestBody Person person) {
+        System.out.println(person);
+        return person;
     }
 }
